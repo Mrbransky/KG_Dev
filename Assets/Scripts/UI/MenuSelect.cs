ab0758e1bf79517e470f25868e43980e0f012d69
Zac-Bruin <zachary.bruin@loop.colum.edu>
2015-11-17 15:22:27 -0600

    Integrated Controls/Instructions
    WHEW

--- Assets/Scripts/UI/MenuSelect.cs
+++ Assets/Scripts/UI/MenuSelect.cs
@@ -2,102 +2,187 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class MenuSelect : MonoBehaviour {
+public class MenuSelect : MonoBehaviour
+{
 
-	public Image option1;
-	public Image option2;
-    public Image option3;
-
-    public Text Credits;
-
+    public Image option1, option2, option3;
     public AudioSource audio;
+    Transform Controls, Instructions, WinCondition;
 
-    float timer = 0.2f;
+    float menuSelectTimer = 0.2f;
+    float instructScreensTimer = 1;
 
     bool canMove = false;
+    bool HasSelectedPlay = false;
+    bool canAdvance = false;
 
-	int choice = 1;
-	// Use this for initialization
-	void Start () {
+    int choice = 1;
+    int WhichInstructScreen = 0;
 
-        audio.loop = true;
-        audio.Play();
-	}
-	// Update is called once per frame
+    void Awake()
+    {
+        Controls = transform.Find("Controls");
+        Instructions = transform.Find("Instructions");
+        WinCondition = transform.Find("WinCondition");
 
-	void Update () {
+        Controls.gameObject.SetActive(false);
+        Instructions.gameObject.SetActive(false);
+        WinCondition.gameObject.SetActive(false);
+    }
 
+    void Update()
+    {
+        DelayMenuSelect();
+        HandleInput();
+        ColorSelectedText();
+
+        if (HasSelectedPlay)
+            ProceedThroughInstructions();
+    }
+
+    void DelayMenuSelect()
+    {
         if (canMove == false)
         {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
+            menuSelectTimer -= Time.deltaTime;
+            if (menuSelectTimer <= 0)
             {
                 canMove = true;
-                timer = 0.2f;
+                menuSelectTimer = 0.2f;
             }
         }
-        float JoystickMove = Input.GetAxisRaw("Menu");
-		if (Input.GetKeyDown (KeyCode.Space)|| Input.GetKey("joystick button 0")) {
-			switch(choice)
-			{
-			case 1:
-				Application.LoadLevel(1);
-				break;
-			case 2:
-                Application.LoadLevel(3);
-				break;
-			case 3:
-				
-                Application.Quit();
-				break;
-			default:
-				break;
-			}
-		}
-		if (Input.GetKeyDown (KeyCode.DownArrow) || JoystickMove >= .7f && canMove == true) {
-			if (choice >=3)
-			{
-                canMove = false;
-				choice = 1;
-			}
-			else{
-            canMove = false;
-			choice++;
-			}
-		}
-        if (Input.GetKeyDown (KeyCode.UpArrow) || JoystickMove <= -.7f && canMove == true)
-        {
-			if (choice <=1)
-			{
-                canMove = false;
-				choice = 3;
-			}
-			else{
-                canMove = false;
-				choice--;
-			}
-		}
+    }
 
-		switch (choice) {
-		
-		case 1:
-			option1.color = Color.black;
-			option2.color = Color.white;
-			option3.color = Color.white;
-			break;
-		case 2:
-			option1.color = Color.white;
-			option2.color = Color.black;
-			option3.color = Color.white;
-			break;
-		case 3:
-			option1.color = Color.white;
-			option2.color = Color.white;
-			option3.color = Color.black;
-			break;
-		default:
-			break;
-		}
-	
-	}
+    void HandleInput()
+    {
+        float JoystickMove = Input.GetAxisRaw("Menu");
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || JoystickMove >= .7f && canMove == true && !HasSelectedPlay)
+        {
+            if (choice >= 3)
+            {
+                canMove = false;
+                choice = 1;
+            }
+            else
+            {
+                canMove = false;
+                choice++;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || JoystickMove <= -.7f && canMove == true && !HasSelectedPlay)
+        {
+            if (choice <= 1)
+            {
+                canMove = false;
+                choice = 3;
+            }
+            else
+            {
+                canMove = false;
+                choice--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKey("joystick button 0"))
+        {
+            if (!HasSelectedPlay)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        HasSelectedPlay = true;
+                        WhichInstructScreen = 1;
+                        Controls.gameObject.SetActive(true);
+
+                        break;
+
+                    case 2:
+                        Application.LoadLevel(3);
+                        break;
+
+                    case 3:
+                        Application.Quit();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    void ColorSelectedText()
+    {
+        switch (choice)
+        {
+            case 1:
+                option1.color = Color.black;
+                option2.color = Color.white;
+                option3.color = Color.white;
+                break;
+
+            case 2:
+                option1.color = Color.white;
+                option2.color = Color.black;
+                option3.color = Color.white;
+                break;
+
+            case 3:
+                option1.color = Color.white;
+                option2.color = Color.white;
+                option3.color = Color.black;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void ProceedThroughInstructions()
+    {
+        DelayInstructScreenAdvance();
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKey("joystick button 0"))
+        {
+            if (canAdvance)
+            {
+                switch (WhichInstructScreen)
+                {
+                    case 1:
+                        WhichInstructScreen++;
+                        instructScreensTimer = 1;
+                        canAdvance = false;
+
+                        Controls.gameObject.SetActive(false);
+                        Instructions.gameObject.SetActive(true);
+                        break;
+
+                    case 2:
+                        WhichInstructScreen++;
+                        instructScreensTimer = 1;
+                        canAdvance = false;
+
+                        Instructions.gameObject.SetActive(false);
+                        WinCondition.gameObject.SetActive(true);
+                        break;
+
+                    case 3:
+                        Application.LoadLevel(1);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    void DelayInstructScreenAdvance()
+    {
+        if (instructScreensTimer > 0)
+            instructScreensTimer -= Time.deltaTime;
+        else
+            canAdvance = true;
+    }
 }
