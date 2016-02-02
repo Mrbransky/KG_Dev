e5d945ecd43aea1b36b66478b3981bbcba8a3fbf
Zac-Bruin <zachary.bruin@loop.colum.edu>
2016-02-02 16:35:27 -0600

    Started Refactoring Player Scripts
    In process of making PlayerControls file BETTER.
    Separated things into separate classes (Entity, Player, etc).
    
    Also created InputMapper Class.

---
+++ Assets/Scripts/Player/PlayerStructure/Ghost.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : Player 
+{
+	public override void Awake() 
+    {
+        FacingRight = false;
+        base.Awake();
+	}
+
+    public override void Update() 
+    {
+        base.Update();
+	}
+}
---
+++ Assets/Scripts/Player/PlayerStructure/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Entity 
+{
+    public BoxCollider2D interactTrigger;
+    public CircleCollider2D bodyCol;
+
+    public int playerNum;
+    protected bool FacingRight;
+
+    public override void Awake() 
+    {
+        interactTrigger = transform.Find("Interact").GetComponent<BoxCollider2D>();
+        bodyCol = GetComponent<CircleCollider2D>();
+        base.Awake();
+	}
+	
+	public override void Update () 
+    {
+        SetMoveDirection();    
+        base.Update();     
+	}
+
+    private void SetMoveDirection()
+    {
+        moveDir = new Vector2((InputMapper.GrabVal(XBOX360_AXES.LeftStick_Horiz, this.playerNum)),
+                             InputMapper.GrabVal(XBOX360_AXES.LeftStick_Vert, this.playerNum));
+    }
+
+    protected void SetFaceDirection()
+    {
+        if (this.transform.localScale.x == 1)
+            FacingRight = false;
+
+        else if (this.transform.localScale.x == -1)
+            FacingRight = true;
+    }
+}
