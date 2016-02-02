73856df145ff6cbab5274e16402bdbda4fd6a89d
Zachary Bruin <zachary.bruin@T3500-Venom.iam.local>
2016-02-02 17:40:09 -0600

    I HOPE THIS WORKS
    NOOT NOOT NOOOT

--- Assets/Scripts/Player/MoveInteractTrigger.cs
+++ Assets/Scripts/Player/MoveInteractTrigger.cs
@@ -19,6 +19,8 @@ public class MoveInteractTrigger : MonoBehaviour {
     {
         PlayerMoveDir = this.GetComponentInParent<Player>().moveDir;
         PlayerFacingRight = this.GetComponentInParent<Player>().FacingRight;
+
+
 	}
 
     //Old Method for moving sprite centered in the middle of InteractTrigger
