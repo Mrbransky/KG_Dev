08101eb034deaaee4ad9b2c2430c62dcc553fb39
Zachary Bruin <zachary.bruin@T3500-Venom.iam.local>
2016-02-11 17:24:50 -0600

    Kissing Sounds
    -Ghost now makes smooch noises (kiss kiss, smek smek)

--- Assets/Scripts/Player/Hierarchy/Ghost.cs
+++ Assets/Scripts/Player/Hierarchy/Ghost.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Ghost : Player
 {
     public float timeBetweenKisses = 1.5f;
     private float timeSinceKiss;
     private MoveInteractTrigger _MoveInteractTrigger;
+    public AudioClip[] smoochSounds;
+
+    private AudioSource source;
 
     public override void Awake() 
     {
@@ -13,6 +16,7 @@ public class Ghost : Player
         base.Awake();
 
         _MoveInteractTrigger = this.interactTrigger.GetComponent<MoveInteractTrigger>();
+        source = this.GetComponent<AudioSource>();
 	}
 
     public override void Update()
@@ -42,6 +46,11 @@ public class Ghost : Player
         return (_MoveInteractTrigger.colliderList.Count > 0 && timeSinceKiss <= 0);
     }
 
+    private AudioClip PickRandomKissSound()
+    {
+        return smoochSounds[Random.Range(0, smoochSounds.Length - 1)];
+    }
+
     private void kissObject()
     {
         // Don't put kiss on cooldown if the furniture is already kissed
@@ -50,6 +59,7 @@ public class Ghost : Player
             if (col.GetComponent<KissableFurniture>().KissFurniture())
             {
                 timeSinceKiss = timeBetweenKisses;
+                source.PlayOneShot(PickRandomKissSound());
             }
         }
     }
