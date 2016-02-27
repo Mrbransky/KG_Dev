0a37f99d7ce0b8feeed5aa858cb2a686636f8a5d
Jack <jack.bransky@gmail.com>
2016-02-27 01:27:33 -0600

    Video framework for main menu

---
+++ KissyGhost/Assets/Scripts/UI/VideoOnDelay.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoOnDelay : MonoBehaviour {
+
+    
+    bool noInputAfterTime = false;
+    float MenuTimer = 5;
+
+	void Update () {
+
+        TimerForVideo();
+
+	}
+
+    void TimerForVideo()
+    {
+        Renderer r = GetComponent<Renderer>();
+        MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+        if (!noInputAfterTime)
+        {
+            MenuTimer -= Time.deltaTime;
+            r.enabled = false;
+
+            if (MenuTimer <= 0)
+            {
+                noInputAfterTime = true;
+                r.enabled = true;
+                
+                MenuTimer = 5;
+
+            }
+        }
+        else
+        {
+            movie.Play();
+        }
+
+
+        if (Input.anyKey)
+        {
+            noInputAfterTime = false;
+            MenuTimer = 5;
+            movie.Stop();
+            r.enabled = false;
+        }
+    }
+}
