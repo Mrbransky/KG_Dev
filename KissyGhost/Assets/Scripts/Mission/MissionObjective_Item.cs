cfa073dfbb0c55ab38dc829e8c0e28d832905bd4
David Sim Wei Lun <davidsimwl@users.noreply.github.com>
2016-02-29 15:20:14 -0600

    Updated key items to turn off their highlight upon being placed on a heartigram

--- KissyGhost/Assets/Scripts/Mission/MissionObjective_Item.cs
+++ KissyGhost/Assets/Scripts/Mission/MissionObjective_Item.cs
@@ -5,7 +5,27 @@ public class MissionObjective_Item : MonoBehaviour
 {
     public int MissionObjectiveListIndex = -1;
     public bool IsItemPlacedDown = false;
+
+    public bool isAnimated = false;
+    public Sprite HighlightedSprite;
+    public Sprite UnhighlightedSprite;
+
+    private bool isHighlighted = true;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
     
+    void Start()
+    {
+        if (isAnimated)
+        {
+            animator = GetComponent<Animator>();
+        }
+        else
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    }
+
     void OnTriggerStay2D(Collider2D col)
     {
         if (IsItemPlacedDown)
@@ -17,6 +37,8 @@ public class MissionObjective_Item : MonoBehaviour
                 itemNodeScript.HasItem = true;
                 GameObject.FindGameObjectWithTag("GameManager").GetComponent<MissionManager>().OnMissionObjectiveCompleted(MissionObjectiveListIndex);
                 transform.position = itemNodeScript.ItemTargetPosition;
+                turnHighlightOff();
+
                 //Item in Heartagram Sound
                 soundManager.SOUND_MAN.playSound("Play_Item_In_Heartagram", gameObject);
 
@@ -27,6 +49,32 @@ public class MissionObjective_Item : MonoBehaviour
 
                 Destroy(GetComponent<Collider2D>());
             }
+        }
+    }
+
+    private void turnHighlightOn()
+    {
+        if (isAnimated)
+        {
+            animator.SetBool("turnHighlightOn", true);
+            animator.SetBool("turnHighlightOff", false);
+        }
+        else
+        {
+            spriteRenderer.sprite = HighlightedSprite;
+        }
+    }
+
+    private void turnHighlightOff()
+    {
+        if (isAnimated)
+        {
+            animator.SetBool("turnHighlightOff", true);
+            animator.SetBool("turnHighlightOn", false);
+        }
+        else
+        {
+            spriteRenderer.sprite = UnhighlightedSprite;
         }
     }
 }
