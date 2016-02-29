d4e30dda684d5cc4f82a17b5885dd64341e3a90d
Zachary Bruin <zachary.bruin@T3500-ToeJam.iam.local>
2016-02-29 10:33:18 -0600

    Folder Organization

---
+++ KissyGhost/Assets/Scripts/UI/OffsetScroller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour
+{
+
+    public float scrollSpeed;
+    private Vector2 savedOffset;
+    public bool goLeft = false;
+
+    void Start()
+    {
+        savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    void Update()
+    {
+        float y = Mathf.Repeat(Time.time * scrollSpeed, 1);
+
+        Vector2 offsetY = new Vector2(savedOffset.x, y);
+        Vector2 offsetX = new Vector2(y, savedOffset.y);
+        if (goLeft)
+        {
+            Vector2 combo = offsetX - offsetY;
+            GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", combo);
+        }
+        else
+        {
+            Vector2 combo = offsetX + offsetY;
+            GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", combo);
+        }
+        
+    }
+
+    void OnDisable()
+    {
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+    }
+}
