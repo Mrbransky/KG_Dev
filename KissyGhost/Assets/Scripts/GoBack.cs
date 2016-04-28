515d178746fa8102e29e3f67f3c743f10ca7a271
Jack Bransky <jack.bransky@gmail.com>
2016-04-28 10:21:33 -0500

    small updates to player select screen

--- KissyGhost/Assets/Scripts/GoBack.cs
+++ KissyGhost/Assets/Scripts/GoBack.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class GoBack : MonoBehaviour {
 
     public HeartZoomTransition _HeartZoomTransition;
-	
+
+    public int sceneToGoBackToo = 0;
 	void Update ()
     {
 	    if (_HeartZoomTransition.enabled)
@@ -15,7 +16,7 @@ public class GoBack : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKey("joystick button 1"))
         {
             _HeartZoomTransition.enabled = true;
-            _HeartZoomTransition.StartHeartZoomIn(0);
+            _HeartZoomTransition.StartHeartZoomIn(sceneToGoBackToo);
         }
 
 #if !UNITY_EDITOR && !UNITY_WEBGL && !UNITY_WEBPLAYER
