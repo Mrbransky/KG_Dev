949f683cae990cc36bef54c3f95f66a283687dab
Jack Bransky <jack.bransky@gmail.com>
2016-05-09 18:05:01 -0500

    The stupidest script I have ever made

---
+++ KissyGhost/Assets/Scripts/STUPIDCATS.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class STUPIDCATS : MonoBehaviour {
+
+    GameObject[] allObjects;
+    public Sprite catSprite;
+	void LateUpdate () {
+        allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.GetComponent<SpriteRenderer>())
+            {
+                obj.GetComponent<SpriteRenderer>().sprite = catSprite;
+            }
+        }
+	}
+}
